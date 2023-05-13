b2e1a88ed0b3a7cf91de11bccd3adfbffed32baf
vinay <vinay@janaspandana.in>
2023-05-13 17:29:35 +0530

    initial commit and project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Drawing App</h1>
+      <p>Development in progress</p>
+    </div>
+  );
+}
+
+export default App;
